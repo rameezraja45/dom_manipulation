cb327e302ad8c5671449237b1128721e3a9650e2
rameezraja45 <raja1999rameez@gmail.com>
2023-02-21 11:13:39 +0530

    commit after queryaelector

--- dom.js
+++ dom.js
@@ -3,20 +3,20 @@
 //console.log(document.domain);
 //console.log(document.URL);
 //console.log(document.title);
-document.title = "Experimenting DOM";
+//document.title = "Experimenting DOM";
 //console.log(document.forms);
 //console.log(document.links);
 
 //GETELEMENTBYID//
 //console.log(document.getElementById('header-title'));
 //var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+//var header = document.getElementById('main-header');
 //console.log(header);
 //headerTitle.textContent = "Hello";
 //headerTitle.innerText = "Hello Buddy";
 //console.log(headerTitle.innerText);
 //headerTitle.innerHTML = '<h3>Hello Guys!</h3>';
-header.style.borderBottom = 'solid 5px #000';
+//header.style.borderBottom = 'solid 5px #000';
 
 
 //GETELEMENTBYCLASSNAME//
@@ -40,7 +40,7 @@ header.style.borderBottom = 'solid 5px #000';
 //GETELEMENTBYTAGNAME//
 //works same as getelementbyclassname//
 
-var li = document.getElementsByTagName('li');
+//var li = document.getElementsByTagName('li');
 //console.log(li);
 //console.log(li[1]);
 //li[1].textContent = 'Hello 2';
@@ -48,11 +48,36 @@ var li = document.getElementsByTagName('li');
 //li[0].style.backgroundColor = 'green';
 
 
-for(var i=0; i<li.length; i++){
-    li[i].style.backgroundColor = '#f4f4f4';
-       li[2].style.backgroundColor = 'green';
-       li[i].style.fontWeight = 'bold';
-   }
-var ttl = document.getElementsByTagName('li');
- ttl[0].style.fontWeight = 'bold';
- ttl[0].style.color = 'green';
+// for(var i=0; i<li.length; i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+//        li[2].style.backgroundColor = 'green';
+//        li[i].style.fontWeight = 'bold';
+//    }
+// var ttl = document.getElementsByTagName('li');
+//  ttl[0].style.fontWeight = 'bold';
+//  ttl[0].style.color = 'green';
+
+
+//QUERYSELECTOR//
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 5px #000';
+
+var input = document.querySelector('input');
+input.value = 'Hello World..';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
+
